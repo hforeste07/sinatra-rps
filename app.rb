f51ebc2c41cab84cf759b8da1cdbd53ffd74cb5a
Heather Forester <hforeste07@gmail.com>
2025-03-11 21:56:10 +0000

    added some code

--- app.rb
+++ app.rb
@@ -2,8 +2,24 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+ erb(:layout)
+ erb(:move)
+end
+
+get("/rock") do
+  moves = ["rock", "paper", "scissors"]
+
+  comp_move = move.sample
+end
+
+get("/scissors") do
+  moves = ["rock", "paper", "scissors"]
+
+  comp_move = move.sample
+end
+
+get("/paper") do
+  moves = ["rock", "paper", "scissors"]
+
+  comp_move = move.sample
 end
